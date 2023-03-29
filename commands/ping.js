cd36db3d4bf9df9c30af5294ae74a020a0dcddc1
HappySnake <bm011989@gmail.com>
2023-03-29 17:32:03 +0200

    added ping command

---
+++ commands/ping.js
@@ -0,0 +1,11 @@
+const { SlashCommandBuilder } = require("discord.js");
+
+module.exports = {
+    data: new SlashCommandBuilder()
+    .setName('ping')
+    .setDescription('Check the bot latency'),
+    async execute(interaction) {
+        await interaction.reply("🏓 **Pong!**")
+        await interaction.editReply(`🏓 **Pong!** (${interaction.client.ws.ping}ms)`)
+    }
+}
