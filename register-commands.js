9b3965de896246fc07958bfdd07263948b24f812
HappySnake <bm011989@gmail.com>
2023-03-29 17:48:29 +0200

    fixed command registering

--- register-commands.js
+++ register-commands.js
@@ -1,35 +1,34 @@
-const fs = require('node:fs')
-const path = require('node:path')
+const fs = require('node:fs');
+const path = require('node:path');
 
-const { REST, Routes } = require('discord.js')
+const { REST, Routes } = require('discord.js');
 
-const { clientId, guildId } = require('./config.json')
 const { token } = require('./token.json')
+const { clientId, guildId } = require('./config.json');
 
-const commands = []
+const commands = [];
 
-const commandsPath = path.join(__dirname, 'commands')
-const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'))
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
 
 for (const file of commandFiles) {
-    const filePath = path.join(commandsPath, file)
-    const command = require(filePath)
-
-    commands.push(command.data.toJSON())
+    const command = require(`./commands/${file}`);
+    commands.push(command.data.toJSON());
 }
 
-const rest = new REST({ version: '10' }).setToken(token)
+const rest = new REST({ version: '10' }).setToken(token);
 
-    (async () => {
-        try {
-            console.log(`Started reloading ${commands.length} commands`)
+(async () => {
+    try {
+        console.log(`Started refreshing ${commands.length} application (/) commands.`);
 
-            const data = await rest.put(
-                Routes.applicationGuildCommands(clientId, guildId), { body: commands }
-            )
+        const data = await rest.put(
+            Routes.applicationGuildCommands(clientId, guildId),
+            { body: commands },
+        );
 
-            console.log(`Success! Reloaded ${data.length} commands`)
-        } catch(error) {
-            console.error(error)
-        }
-})()
+        console.log(`Successfully reloaded ${data.length} application (/) commands.`);
+    } catch (error) {
+        console.error(error);
+    }
+})();
