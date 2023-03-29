8ed29cefeb9f601443dbc0bea88b7f8e362b6169
HappySnake <bm011989@gmail.com>
2023-03-29 17:37:34 +0200

    added command handling

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ const path = require('node:path')
 const { Client, Events, GatewayIntentBits, Collection } = require('discord.js')
 const { token } = require('./config.json')
 
-const client = new Client({ intents: [ GatewayIntentBits.Guilds ]});
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 client.commands = new Collection()
 
 const commandsPath = path.join(__dirname, 'commands')
 const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'))
 
-for(const file of commandFiles) {
+for (const file of commandFiles) {
     const filePath = path.join(commandsPath, file)
     const command = require(filePath)
 
@@ -26,9 +26,25 @@ client.once(Events.ClientReady, () => {
 })
 
 client.on(Events.InteractionCreate, async (interaction) => {
-    if(!interaction.isChatInputCommand()) return
+    if (!interaction.isChatInputCommand()) return
 
-    console.log(interaction)
+    const comamnd = interaction.client.commands.get(interaction.commandName)
+
+    if (!command) {
+        console.error(`Found no command matching ${interaction.commandName}`)
+        return
+    }
+
+    try {
+        await command.execute(interaction)
+    } catch (error) {
+        console.error(error)
+        if (interaction.replied || interaction.deferred) {
+            await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
+        } else {
+            await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+        }
+    }
 
 })
 
