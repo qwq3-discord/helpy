2eaee26fd94ece54bfd979fa94dc0ab2e1bad2b8
HappySnake <bm011989@gmail.com>
2023-03-29 17:39:29 +0200

    using new token

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const fs = require('node:fs')
 const path = require('node:path')
 
 const { Client, Events, GatewayIntentBits, Collection } = require('discord.js')
-const { token } = require('./config.json')
+const { token } = require('./token.json')
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 client.commands = new Collection()
