a4159a8fb88caa13da855821ef604d94043e993d
zHappySnake <bm011989@gmail.com>
2023-03-30 16:19:18 +0200

    event handler

--- index.js
+++ index.js
@@ -21,9 +21,23 @@ for (const file of commandFiles) {
     }
 }
 
-client.once(Events.ClientReady, () => {
-    console.log(`Ready and logged in as ${client.user.tag}`)
-})
+const eventsPath = path.join(__dirname, 'events')
+const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'))
+
+for (const file of eventFiles) {
+    const filePath = path.join(eventsPath, file)
+    const event = require(filePath)
+
+    if(event.once) {
+        client.once(event.name, (...args) => event.execute(...args))
+    } else {
+        client.on(event.name, (...args) => event.execute(...args))
+    }
+}
+
+// client.once(Events.ClientReady, () => {
+//     console.log(`Ready and logged in as ${client.user.tag}`)
+// })
 
 client.on(Events.InteractionCreate, async (interaction) => {
     if (!interaction.isChatInputCommand()) return
